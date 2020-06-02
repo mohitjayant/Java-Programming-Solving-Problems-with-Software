81f0b10c28a6971ce0ce6e47e5072f3b45c724b8
mohitj8790 <mohitj8790@gmail.com>
2020-06-02 19:00:33 +0530

    Adding new feature

--- Week3/countryExport.java
+++ Week3/countryExport.java
@@ -6,9 +6,11 @@ public class countryExport {
 
     public void listExport(CSVParser parser,String exportInterest)
     {
+        System.out.println("Country which export a specific product: ");
         for(CSVRecord record:parser)
         {
             String export=record.get("Exports");
+            
             if(export.contains(exportInterest))
             {
                 String country=record.get("Country");
@@ -19,10 +21,12 @@ public class countryExport {
 
     public void countryInfo(CSVParser parser,String country) {
         String res="";
+        System.out.println("Conuntry Exports and Value: ");
         for(CSVRecord record:parser)
         {
             
             String co=record.get("Country");
+            
             if(co.contains(country))
             {
                 res=co+": "+record.get("Exports")+": "+record.get("Value (dollars)");
@@ -37,13 +41,30 @@ public class countryExport {
         }
     }
 
+    public void listExportersTwoProducts(CSVParser parser,String product1,String product2) 
+    {
+        System.out.println("Countries which export both Products: ");
+        for(CSVRecord record:parser)
+        {
+            String product=record.get("Exports");
+            
+            if(product.contains(product1) && product.contains(product2))
+            {
+                System.out.println(record.get("Country"));
+            }
+        }
+    }
+
     public void testListExport()
     {
         FileResource fr=new FileResource();
         CSVParser parser=fr.getCSVParser();
 
-        //listExport(parser,"sugar");
+        listExport(parser,"diamonds");
+        parser=fr.getCSVParser();
         countryInfo(parser,"Germany");
+        parser=fr.getCSVParser();
+        listExportersTwoProducts(parser,"gold","diamonds");
     }
 
     public static void main(String[] args) {
