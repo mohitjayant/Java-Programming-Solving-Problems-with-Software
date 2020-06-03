8c4a6d4c2a6e5229b31445cea4670fd050d569d0
mohitj8790 <mohitj8790@gmail.com>
2020-06-03 08:46:45 +0530

    Feature of maxm temperature calculation from many CSV file-Increasing Reusability of code

---
+++ Week3/weatherCondition.java
@@ -0,0 +1,141 @@
+package Week3;
+import edu.duke.*;
+import java.io.*;
+import org.apache.commons.csv.*;
+
+public class weatherCondition {
+
+    public CSVRecord minmOfTwo(CSVRecord currentRow,CSVRecord minmSoFar) 
+    {
+        if(minmSoFar==null)
+            {
+                minmSoFar=currentRow;
+            }
+        else
+        {
+
+            if(currentRow.get("Humidity").contains("N/A"))
+            {
+                currentRow=minmSoFar;
+            }
+            double current=Double.parseDouble(currentRow.get("Humidity"));
+            double largest=Double.parseDouble(minmSoFar.get("Humidity"));
+            if(current==-9999.0)
+            {
+                current=largest;
+            }
+
+            if(current<largest)
+            {
+                minmSoFar=currentRow;
+            }
+        }
+        return minmSoFar;
+    }
+
+    public CSVRecord calMaxTemp(CSVParser parser)
+    {
+        CSVRecord minmSoFar=null;
+        for(CSVRecord currentRow:parser)
+        {
+            minmSoFar=minmOfTwo(currentRow, minmSoFar);
+        }
+
+        return minmSoFar;
+    }
+
+    public double avgTemperature(CSVParser parser) 
+    {
+        double avg=0.0;
+        int count=0;
+        for(CSVRecord record:parser)
+        {
+            if(record.get("Humidity").contains("N/A"))
+            {
+                continue;
+            }
+            if(Double.parseDouble(record.get("Humidity"))>80.0)
+            {
+                double temp=Double.parseDouble(record.get("TemperatureF"));
+                count++;
+                avg+=temp;
+            }
+        }
+        avg=avg/count;
+        return avg;
+    }
+
+    public void testSingleTemp() 
+    {
+        FileResource fr=new FileResource();
+        CSVParser parser=fr.getCSVParser();
+
+        CSVRecord temp=calMaxTemp(parser);
+        parser=fr.getCSVParser();
+        double temperature=avgTemperature(parser);
+        System.out.println("Avg. Temperature: "+ temperature + " at "+temp.get("DateUTC"));
+        System.out.println("Minm. Temperature: "+temp.get("TemperatureF") + " at "+temp.get("DateUTC"));
+    }
+
+    public void testManyDaysTemp() 
+    {
+         CSVRecord largestSoFar=null;
+         DirectoryResource dr=new DirectoryResource();
+
+         for(File f:dr.selectedFiles())
+         {
+             FileResource fr=new FileResource(f);
+             CSVRecord currentRow=calMaxTemp(fr.getCSVParser());
+             largestSoFar=minmOfTwo(currentRow, largestSoFar);
+         }
+         System.out.println("Maxm. Temperature: "+Double.parseDouble(largestSoFar.get("TemperatureF")) + " at "+largestSoFar.get("DateUTC"));
+        
+    }
+
+    public CSVRecord lowestHumidityInFile(CSVParser parser)
+    {
+        CSVRecord minmSoFar=null;
+        for(CSVRecord currentRow:parser)
+        {
+            minmSoFar=minmOfTwo(currentRow, minmSoFar);
+        }
+
+        return minmSoFar;
+    }
+
+    public void testLowestHumidityInFile() 
+    {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        CSVRecord csv = lowestHumidityInFile(parser);
+        System.out.println("Minm. Humidity: "+csv.get("Humidity") + " at "+csv.get("DateUTC"));
+    }
+
+    public void testLowestHumidityInManyFile() 
+    {
+         CSVRecord largestSoFar=null;
+         DirectoryResource dr=new DirectoryResource();
+
+         for(File f:dr.selectedFiles())
+         {
+             FileResource fr=new FileResource(f);
+             CSVRecord currentRow=lowestHumidityInFile(fr.getCSVParser());
+             largestSoFar=minmOfTwo(currentRow, largestSoFar);
+         }
+         System.out.println("Maxm. Humidity: "+Double.parseDouble(largestSoFar.get("Humidity")) + " at "+largestSoFar.get("DateUTC"));
+        
+    }
+
+    public static void main(String[] args) {
+        weatherCondition mt=new weatherCondition();
+        //Function for maxm Temperature in a day
+        mt.testSingleTemp();
+        //Function for maxm Temperature in many days
+        //mt.testManyDaysTemp();
+        //Test lowest humidity
+        //mt.testLowestHumidityInFile();
+        //mt.testLowestHumidityInManyFile();
+
+    }
+    
+};
