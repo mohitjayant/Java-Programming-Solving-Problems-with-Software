514a155a888814c022a7a1f5405619003f4c2029
mohitj8790 <mohitj8790@gmail.com>
2020-06-04 00:11:23 +0530

    adding feature of total names, total girls and total boys in file

--- Week4/babyBirthsName.java
+++ Week4/babyBirthsName.java
@@ -4,14 +4,35 @@ import org.apache.commons.csv.*;
 
 public class babyBirthsName 
 {
+    public void totalBirths(FileResource fr) 
+    {
+        int totalBir=0;
+        int totalBoys=0;
+        int totalGirls=0;
+        for(CSVRecord rec:fr.getCSVParser(false))
+        {
+            int numBorn=Integer.parseInt(rec.get(2)); 
+            totalBir+=numBorn;
+            if(rec.get(1).equals("M"))
+            {
+                totalBoys+=numBorn;
+            } 
+            else
+            {
+                totalGirls+=numBorn;
+            }
+        }
+        System.out.println("Total Birth: "+totalBir);
+        System.out.println("Total Girls: "+totalGirls);
+        System.out.println("Total Boys: "+totalBoys);
+
+    }
 
     public void printNames() 
     {
         FileResource fr=new FileResource();
-        for(CSVRecord rec:fr.getCSVParser(false))
-        {
-            System.out.println("Names "+rec.get(0)+" Gender "+rec.get(1)+" Num. Born "+rec.get(2));
-        }
+        totalBirths(fr);
+        
     }
 
     public static void main(String[] args) {
