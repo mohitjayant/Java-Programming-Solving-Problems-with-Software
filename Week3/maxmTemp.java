3a5d3284259c231e65a4d7084d854cb54532fcac
mohitj8790 <mohitj8790@gmail.com>
2020-06-03 06:18:20 +0530

    Feature of maxm temperature calculation from a CSV file

---
+++ Week3/maxmTemp.java
@@ -0,0 +1,46 @@
+package Week3;
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class maxmTemp {
+
+    public CSVRecord calMaxTemp(CSVParser parser)
+    {
+        CSVRecord largestSoFar=null;
+        for(CSVRecord currentRow:parser)
+        {
+            if(largestSoFar==null)
+            {
+                largestSoFar=currentRow;
+            }
+            else
+            {
+                double currentTemp=Double.parseDouble(currentRow.get("TemperatureF"));
+                double largesttemp=Double.parseDouble(currentRow.get("TemperatureF"));
+
+                if(currentTemp>largesttemp)
+                {
+                    largestSoFar=currentRow;
+                }
+            }
+        }
+
+        return largestSoFar;
+    }
+
+    public void testTemp() 
+    {
+        FileResource fr=new FileResource();
+        CSVParser parser=fr.getCSVParser();
+
+        CSVRecord temp=calMaxTemp(parser);
+        double temperature=Double.parseDouble(temp.get("TemperatureF"));
+        System.out.println("Maxm. Temperature: "+temperature);
+    }
+
+    public static void main(String[] args) {
+        maxmTemp mt=new maxmTemp();
+        mt.testTemp();
+    }
+    
+};
